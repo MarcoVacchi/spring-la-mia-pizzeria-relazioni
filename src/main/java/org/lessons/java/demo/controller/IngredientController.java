fc12e0b16d4b29c0fd0250a7463da9c2a2c8a215
MarcoVacchi <mvacchi816@gmail.com>
2025-08-19 21:15:04 +0200

    feat: added ingredient route

--- src/main/java/org/lessons/java/demo/controller/IngredientController.java
+++ src/main/java/org/lessons/java/demo/controller/IngredientController.java
@@ -1,10 +1,16 @@
 package org.lessons.java.demo.controller;
 
+import org.lessons.java.demo.model.Ingredient;
 import org.lessons.java.demo.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -13,8 +19,26 @@ public class IngredientController {
     @Autowired
     private IngredientRepository ingredientRepository;
 
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("ingredient", ingredientRepository.findAll());
+        model.addAttribute("ingredients", ingredientRepository.findAll());
         return "ingredients/index";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
+        return "ingredients/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "ingredients/create-or-edit";
+        }
+        ingredientRepository.save(formIngredient);
+        return "redirect:/ingredients";
+    }
+
 }
