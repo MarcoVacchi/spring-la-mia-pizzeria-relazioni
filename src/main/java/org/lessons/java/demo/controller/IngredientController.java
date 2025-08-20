48fa585770b80732ab8824e6de9971116216d379
MarcoVacchi <mvacchi816@gmail.com>
2025-08-20 10:26:45 +0200

    feat: added edit and create

--- src/main/java/org/lessons/java/demo/controller/IngredientController.java
+++ src/main/java/org/lessons/java/demo/controller/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,12 @@ public class IngredientController {
         return "ingredients/index";
     }
 
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("ingredient", ingredientRepository.findById(id).get());
+        return "ingredients/show";
+    }
+
     @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("ingredient", new Ingredient());
@@ -33,6 +40,23 @@ public class IngredientController {
 
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "ingredients/create-or-edit";
+        }
+        ingredientRepository.save(formIngredient);
+        return "redirect:/ingredients";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("ingredient", ingredientRepository.findById(id).get());
+        model.addAttribute("edit", true);
+        return "ingredients/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("ingredients") Ingredient formIngredient, BindingResult bindingResult,
             Model model) {
         if (bindingResult.hasErrors()) {
             return "ingredients/create-or-edit";
